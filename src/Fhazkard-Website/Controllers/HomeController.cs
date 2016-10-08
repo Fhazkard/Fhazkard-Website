f4605b25f20e11e9e4616c77c80ca26faaa25457
user <user@USER-PC>
2016-10-08 16:19:16 +0700

    Add project files.

---
+++ src/Fhazkard-Website/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fhazkard_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Blog");
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "My profile description page";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "My contact page";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
