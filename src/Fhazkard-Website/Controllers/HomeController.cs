875fefacfff5be61bf998a3d28a3908614b00744
user <user@USER-PC>
2016-10-10 08:42:40 +0700

    Fix Sistem Index & CSS

--- src/Fhazkard-Website/Controllers/HomeController.cs
+++ src/Fhazkard-Website/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Fhazkard_Website.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fhazkard_Website.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        private readonly ApplicationDbContext _context;
 
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Blog.ToListAsync());
         }
 
         public IActionResult About()
